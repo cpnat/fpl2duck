ce360374148688d073915337ed03d989963fc4c5
cpnat <cpnattrass@googlemail.com>
2024-03-24 21:04:37 +0100

    Add codemirror

--- fpl2duck-frontend/main.js
+++ fpl2duck-frontend/main.js
@@ -6,15 +6,41 @@ import duckdb_wasm_next from '@duckdb/duckdb-wasm/dist/duckdb-eh.wasm?url';
 import eh_worker from '@duckdb/duckdb-wasm/dist/duckdb-browser-eh.worker.js?url';
 import Handsontable from 'handsontable';
 import 'handsontable/dist/handsontable.full.min.css';
+import { EditorView, keymap } from '@codemirror/view';
+import { EditorState } from '@codemirror/state';
+import { defaultKeymap } from '@codemirror/commands';
+import { basicSetup } from "codemirror"
+import { sql } from '@codemirror/lang-sql';
 
 document.title = "⚽ FPL2Duck 🦆";
 
+
 document.querySelector('#app').innerHTML = `
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   <h1>⚽ Hello Ducklovers 🦆</h1>
-  <div class="form-floating">
-    <textarea placeholder="Enter your query" id="query" name="query" rows="30" cols="100"></textarea>
+  <div id="accordion">
+    <div class="card">
+      <div class="card-header" id="headingOne">
+        <h5 class="mb-0">
+          <button class="btn btn-link" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+          Overview
+          </button>
+        </h5>
+      </div>
+
+    <div id="collapseOne" class="collapse" aria-labelledby="headingOne" data-parent="#accordion">
+      <div class="card-body">
+        Run SQL queries on the FPL database. The database is loaded with the latest data from the FPL API.<br>
+        Example queries:<br>
+        <i>SHOW TABLES; // Show all tables in the database</i><br>
+        <i>SELECT * FROM element_history WHERE element IN (SELECT id FROM elements WHERE web_name = 'Ødegaard') ORDER BY kickoff_time // Get all matches of a player</i><br>
+
+      </div>
+    </div>
   </div>
+</div>
+<div id="queryBox">
+  <div id="queryEditor"></div>
+</div>
   <button type="button" id="submit" class="btn btn-success">Submit</button><br>
   <div id="result"></div>
 `;
@@ -70,18 +96,59 @@ async function loadDatabaseFiles(conn, baseURL) {
   await loadFileToDatabase(conn, dbDataUrl, baseURL);
 }
 
+
+function setupEditor() {
+  const queryTextArea = document.getElementById('queryEditor');
+
+  const queryEditor = new EditorView({
+    state: EditorState.create({
+      doc: queryTextArea.value,
+      values: startingValue,
+      extensions: [
+        basicSetup,
+        sql(),
+      ]
+    }),
+  parent: queryTextArea
+  });
+
+  var minLines = 3;
+  var startingValue = '';
+  for (var i = 0; i < minLines; i++) {
+    startingValue += '\n';
+  }
+
+  // queryEditor.setValue(startingValue);
+
+  return queryEditor;
+}
+
+
 async function init() {
   console.log('Initializing DuckDB');
   const conn = await setupDatabase();
   const baseURL = new URL('./db_export', import.meta.url).href;
 
+  console.log('Loading database files');
   await loadDatabaseFiles(conn, baseURL);
-  document.getElementById('submit').addEventListener("click", () => submitQuery(conn));
+
+  const queryEditor = setupEditor();
+
+  document.getElementById('submit').addEventListener("click", () => submitQuery(conn, queryEditor));
 }
 
-async function submitQuery(conn) {
-  const query = document.getElementById('query').value;
+// Get the query text from the CodeMirror editor
+function getQuery(queryEditor) {
+  console.log('Query:', queryEditor.state.doc.toString());
+  return queryEditor.state.doc.toString();
+}
+
+async function submitQuery(conn, queryEditor) {
+  const query = getQuery(queryEditor);
   console.log("Submitting query:", query);
+
+  const container = document.getElementById('result');
+  container.innerHTML = '';
 
   try {
     const res = await conn.query(query);
@@ -96,9 +163,6 @@ async function submitQuery(conn) {
     const columnHeaders = Object.keys(data[0]);
     console.log('Query result copy (JSON):', JSON.parse(JSON.stringify(data)));
 
-    const container = document.getElementById('result');
-    container.innerHTML = '';
-
     const hot = new Handsontable(container, {
       data: data,
       readOnly: true,
@@ -110,6 +174,7 @@ async function submitQuery(conn) {
       licenseKey: 'non-commercial-and-evaluation' // for non-commercial use only
     });
   } catch (error) {
+    container.innerHTML = 'Error executing query: ' + error;
     console.error('Error executing query:', error);
   }
 }
